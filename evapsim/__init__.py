e93bacd1065aba9fc7edd0fd3586671eac295a58
Adam Thomas <1adamt12@gmail.com>
2022-06-09 15:12:06 -0400

    Fix __init__ so physics can be imported without running simulator

--- evapsim/__init__.py
+++ evapsim/__init__.py
@@ -2,18 +2,18 @@
 from evapsim import simulator
 import logging
 
+if __name__ == '__main__':
+    logging.basicConfig(filename='Evap_Sim.log',
+                        encoding='utf-8',
+                        level=logging.INFO,
+                        format='%(asctime)s %(message)s',
+                        filemode='w')
+    log = logging.getLogger(__name__)
+    log.addHandler(logging.NullHandler())
 
-logging.basicConfig(filename='Evap_Sim.log',
-                    encoding='utf-8',
-                    level=logging.INFO,
-                    format='%(asctime)s %(message)s',
-                    filemode='w')
-log = logging.getLogger(__name__)
-log.addHandler(logging.NullHandler())
+    stream = logging.StreamHandler()
+    stream.setLevel(logging.DEBUG)
+    log.addHandler(stream)
 
-stream = logging.StreamHandler()
-stream.setLevel(logging.DEBUG)
-log.addHandler(stream)
-
-log.info("Program Start")
-sim = simulator.Simulator()
+    log.info("Program Start")
+    sim = simulator.Simulator()
