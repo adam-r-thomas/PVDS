5124dcd8d2eee10f713b58e8968e9de5f27fabed
Adam Thomas <83033536+ColdAdam@users.noreply.github.com>
2023-01-04 12:37:14 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="evapsim",
-    version="0.0.5",
+    version="0.0.6",
     description="Evaporation simulator for thermal, ebeam, sputter",
     author="Adam R Thomas",
     classifiers=["Programming Language :: Python :: 3.10"],
